e5193cd03c64b5fd9e13d68f93e1401e1d61855c
Dae Hyeon <daehyeon1999@naver.com>
2022-04-13 20:35:14 +0800

    Add: add delete empolyee

--- lib/mobile_app/home.dart
+++ lib/mobile_app/home.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:ndialog/ndialog.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:work_record_app/mobile_app/add_employee.dart';
 import 'package:work_record_app/mobile_app/employee_record.dart';
@@ -10,47 +11,49 @@ class MobileHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            //Logo
-            SizedBox(
-                width: 250,
-                child: Image.asset(
-                  'assets/mashiso.png',
-                )),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              //Logo
+              SizedBox(
+                  width: 250,
+                  child: Image.asset(
+                    'assets/mashiso.png',
+                  )),
 
-            //
-            const SizedBox(
-              height: 15,
-            ),
+              //
+              const SizedBox(
+                height: 15,
+              ),
 
-            //Employee text and add button
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                const Text(
-                  "Employee",
-                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-                ),
+              //Employee text and add button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const Text(
+                    "Employee",
+                    style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                  ),
 
-                //Add Employee
-                IconButton(
-                  onPressed: () => Navigator.push(
-                      context,
-                      PageTransition(
-                          child: const AddEmployee(),
-                          type: PageTransitionType.leftToRight)),
-                  icon: const Icon(Icons.person_add),
-                  color: const Color(0xffFDBF05),
-                )
-              ],
-            ),
+                  //Add Employee
+                  IconButton(
+                    onPressed: () => Navigator.push(
+                        context,
+                        PageTransition(
+                            child: const AddEmployee(),
+                            type: PageTransitionType.leftToRight)),
+                    icon: const Icon(Icons.person_add),
+                    color: const Color(0xffFDBF05),
+                  )
+                ],
+              ),
 
-            //Employee list
-            const EmployeeList()
-          ],
+              //Employee list
+              const EmployeeList()
+            ],
+          ),
         ),
       ),
     );
@@ -105,40 +108,64 @@ class _EmployeeListState extends State<EmployeeList> {
             physics: const ScrollPhysics(),
             itemCount: numOfEmployees,
             itemBuilder: (BuildContext context, int index) {
-              return InkWell(
-                onTap: () {
-                  // go to the record Page
-                  Navigator.push(
+              return Card(
+                elevation: 3,
+                color: const Color(0xffFDBF05),
+                child: ListTile(
+                  title: Center(
+                      child: Text(
+                    "${nameList[index]}",
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontSize: 30, fontWeight: FontWeight.bold),
+                  )),
+                  onTap: () => Navigator.push(
                       context,
                       PageTransition(
                           child: EmployeeRecord(
-                            employeeId: idList[index],
-                            name: nameList[index],
-                          ),
-                          type: PageTransitionType.rightToLeftWithFade));
-                },
-                highlightColor: Colors.transparent,
-                splashFactory: NoSplash.splashFactory,
-                child: Card(
-                    elevation: 5,
-                    color: const Color(0xffFDBF05),
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20))),
-                    child: SizedBox(
-                      height: 80,
+                              employeeId: idList[index], name: nameList[index]),
+                          type: PageTransitionType.rightToLeftWithFade)),
+                  onLongPress: () => NAlertDialog(
+                    content: const SizedBox(
+                      height: 70,
                       child: Center(
-                        // Name of the Employee
-                        child: Padding(
-                          padding: const EdgeInsets.all(5.0),
                           child: Text(
-                            "${nameList[index]}",
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                                fontSize: 30, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ),
-                    )),
+                        "Are you sure to delete his employee?",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w500),
+                      )),
+                    ),
+                    actions: [
+                      TextButton(
+                          style: const ButtonStyle(
+                              splashFactory: NoSplash.splashFactory),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text(
+                            "Cancel",
+                            style: TextStyle(color: Colors.black),
+                          )),
+                      TextButton(
+                          onPressed: () async {
+                            await FirebaseFirestore.instance
+                                .collection('employee')
+                                .doc(idList[index])
+                                .delete();
+
+                            Navigator.pushReplacement(
+                                context,
+                                PageTransition(
+                                    child: const MobileHome(),
+                                    type: PageTransitionType.fade));
+                          },
+                          child: const Text(
+                            "Ok",
+                            style: TextStyle(color: Colors.red),
+                          )),
+                    ],
+                  ).show(context),
+                ),
               );
             },
           ),
